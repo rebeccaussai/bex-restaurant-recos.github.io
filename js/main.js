477f6b1b01a2bdb0c0290e88e5cc5d6d374fbd2f
Sonyl Nagale <sonyl.nagale@generalassemb.ly>
2018-03-22 10:28:14 -0400

    Suggestions to unblock

--- js/main.js
+++ js/main.js
@@ -4,10 +4,25 @@ var source = $('#restaurant-template').html();
 // 2. use handlebars compile
 var template = Handlebars.compile(source);
 
+var restaurants = [
+	{
+		"name": "Big Star",
+		"image": "images/big_star.jpg",
+		"description": "description of big star",
+		"address": "123 Main St. Chicago, IL 60614",
+		"hours": "API data11 am - 11 pm"
+	}
+];
+
+var request = {
+  placeId: 'ChIJwwvDb8fSD4gRb54qO3OOpVA'
+};
+
+
 
 // 3. is this where my object constructor goes????
 //constructor function to create a restaurant tile object
-var restaurant = function (name, image, description, address, hours) {
+var Restaurant = function (name, image, description, address, hours) {
 	this.name = name;
 	this.image = image;
 	this.description = description;
@@ -16,13 +31,23 @@ var restaurant = function (name, image, description, address, hours) {
 };
 
 // 4. pass it into template
-var newTile = template(restaurant);
 
 // add a new tile
-var bigStar = new restaurant("Big Star", "images/big_star.jpg", "description of big star", "123 Main St. Chicago, IL 60614", "API data11 am - 11 pm");
+for (var i = 0; i < restaurants.length; i++) {
+	var r = restaurants[i];
 
-// append html to the DOM 
-$('#allTiles').html(newTile);
+	var restaurant = new Restaurant(r.name, r.image, r.description, r.address, r.hours);
+
+	var newTile = template(restaurant);
+
+	$('#allTiles').append(newTile);
+
+}
+//var bigStar = new Restaurant("Big Star", "images/big_star.jpg", "description of big star", "123 Main St. Chicago, IL 60614", "API data11 am - 11 pm");
+
+// var newTile = template(bigStar);
+
+// append html to the DOM
 
 
 //how do I tie to HTML?
@@ -39,7 +64,7 @@ function initMap() {
       lat: position.coords.latitude,
       lng: position.coords.longitude
     };
-    
+
     // create a new instance of a map
     // configure map with options object
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -47,7 +72,7 @@ function initMap() {
     	zoom: 10,
     	scrollwheel: false
  	});
-    
+
     // create a new marker
     var marker = new google.maps.Marker({
       position: {lat: 41.909368, lng: -87.677111},
@@ -56,6 +81,16 @@ function initMap() {
     });
 
   });
-}
 
-initMap();
+
+	var service = new google.maps.places.PlacesService(map);
+	service.getDetails(request, callback);
+
+	function callback(place, status) {
+	  // if (status == google.maps.places.PlacesServiceStatus.OK) {
+	  //   createMarker(place);
+	  // }
+
+		console.log(place);
+	}
+}
